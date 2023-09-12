e64c36882f9a9b756119e3f5305dec533af2301f
rajesh <rajeshchandran007@gmail.com>
2023-09-12 16:11:45 +0530

    ok

--- if.sh
+++ if.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-mynum=200
+mynum1=200
+if [ $mynum1 -eq 200 ]
+then echo "Yes mynum1 is 200"
+else echo "No, mynum1 is not 200"
+fi
+echo "----------------"
+mynum=300
 if [ $mynum -eq 200 ]
-then echo "Yes it is 200"
-else echo "No, its not 200"
+then echo "Yes mynum1 is 200"
+else echo "No, mynum1 is not 200"
 fi
